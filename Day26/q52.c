70938b5dd353c557e80c8b06622e6667bbf3cb96
Saranya2233 <Saranya.41598@stu.upes.ac.in>
2026-09-25 08:14:45 +0000

    Day 26 Q51 and Q52

---
+++ Day26/q52.c
@@ -0,0 +1,52 @@
+/* 
+ * Name : Saranya
+ * Roll : 590041598
+ * Day : 26 Question: 52
+ * Date : 25-09-2026
+ * 
+ * PROBLEM STATEMENT:Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 4; i++)
+        printf("*\n");
+
+    printf("\n");
+
+    for (i = 1; i <= 5; i++)
+        printf("*\n");
+
+    printf("\n");
+
+    for (i = 1; i <= 3; i++)
+        printf("*\n");
+
+    printf("\n");
+
+    for (i = 1; i <= 1; i++)
+        printf("*\n");
+
+    return 0;
+}
