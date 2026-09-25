fbe3e4ad198a84b73f36c2822b86cc384f55542f
Saranya2233 <Saranya.41598@stu.upes.ac.in>
2026-09-25 00:34:02 +0000

    Day 16 Q31 and Q32

---
+++ Day16/q31.c
@@ -0,0 +1,29 @@
+/* 
+ * Name : Saranya
+ * Roll : 590041598
+ * Day : 16 Question: 31
+ * Date : 25-09-2026
+ * 
+ * PROBLEM STATEMENT:Write a program to take a number as input and print its equivalent binary representation.
+ */
+#include <stdio.h>
+
+int main()
+{
+    int n, binary = 0, place = 1, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        remainder = n % 2;
+        binary = binary + remainder * place;
+        n = n / 2;
+        place = place * 10;
+    }
+
+    printf("Binary representation = %d", binary);
+
+    return 0;
+}
